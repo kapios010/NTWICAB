7c6900fb7d162c5c3950e726a605d2c3a3304f5d
Kapios010 <67257824+Kapios010@users.noreply.github.com>
2026-06-20 20:35:19 +0200

    Add Title Banner

--- NTWICAB/Program.cs
+++ NTWICAB/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace NTWICAB
+﻿using Spectre.Console;
+using System.Text;
+
+namespace NTWICAB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            var larry3d = FigletFont.Load(new MemoryStream(Resources.chunky));
+            AnsiConsole.Write(new FigletText(larry3d, "NTWICAB") { Justification = Justify.Center, Color = Color.Chartreuse1 });
+            AnsiConsole.Write(new Markup("[chartreuse1]Now That's What I Call A Broadcast[/]\n") { Justification = Justify.Center});
+            AnsiConsole.Write(new Rule{ Justification = Justify.Center, Style = Style.Parse("chartreuse1 dim")});
         }
     }
 }
